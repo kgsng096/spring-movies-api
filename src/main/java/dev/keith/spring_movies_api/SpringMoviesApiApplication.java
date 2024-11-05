41950e5e6933c694b7416311d829d1319a81d30c
kgsng096 <keithng.096@gmail.com>
2024-11-05 10:31:11 +0800

    init

---
+++ src/main/java/dev/keith/spring_movies_api/SpringMoviesApiApplication.java
@@ -0,0 +1,13 @@
+package dev.keith.spring_movies_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMoviesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMoviesApiApplication.class, args);
+	}
+
+}
