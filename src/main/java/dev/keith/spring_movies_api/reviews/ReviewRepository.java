5f77769dc2f4434aa056d6ae2e7ce86f353bb0a8
kgsng096 <keithng.096@gmail.com>
2024-11-05 14:17:11 +0800

    added review feature

---
+++ src/main/java/dev/keith/spring_movies_api/reviews/ReviewRepository.java
@@ -0,0 +1,9 @@
+package dev.keith.spring_movies_api.reviews;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
